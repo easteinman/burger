96645647cf463043ea557497ff3041270be03b3d
Eric <ericsteinman@me.com>
2018-03-12 15:07:21 -0500

    First commit. Getting all the files set up per the instructions.

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+-- Drops the burgers_db if it exists currently --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Creates the "burgers_db" database --
+CREATE DATABASE burgers_db;
+
+-- Makes it so all of the following code will affect burgers_db --
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(40) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
